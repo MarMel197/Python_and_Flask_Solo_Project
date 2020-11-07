9402b1ebbe1c138134e3c944d9151443c7ae3965
Mark <markmeldrum70@gmail.com>
2020-11-07 12:26:00 +0000

    Product tests complete, working

--- project_1/run_tests.py
+++ project_1/run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from tests.product_test import TestProduct
-
+from tests.manufacturer_test import TestManufacturer
 
 if __name__ == '__main__':
     unittest.main()
--- project_1/tests/product_test.py
+++ project_1/tests/product_test.py
@@ -5,12 +5,44 @@ class TestProduct(unittest.TestCase):
     
     def setUp(self):
         self.product = Product('Vegetable Soup', 'Tinned Products', 12, 0.65, 0.90)
+        self.product1 = Product('Noodle soup', 'Tinned Products', 24, 0.45, 0.75)
     
-    def test_product_has_description(self):
+    def test_product_has_name(self):
         self.assertEqual('Vegetable Soup', self.product.product_name)
+    
+    def test_another_product_has__name(self):
+        self.assertEqual('Noodle soup', self.product1.product_name)
 
+    def test_product_has_description(self):
+        self.assertEqual('Tinned Products', self.product.product_description)
+    
+    def test_another_product_has__description(self):
+        self.assertEqual('Tinned Products', self.product1.product_description)
 
+    def test_product_has_stock_on_hand(self):
+        self.assertEqual(12 , self.product.stock_on_hand)
+    
+    def test_another_product_has__stock_on_hand(self):
+        self.assertEqual(24 , self.product1.stock_on_hand)
 
+    def test_product_has_cost_price(self):
+        self.assertEqual(0.65 , self.product.cost_price)
+    
+    def test_another_product_has__cost_price(self):
+        self.assertEqual(0.45 , self.product1.cost_price)
+
+    def test_product_has_sell_price(self):
+        self.assertEqual(0.90 , self.product.sell_price)
+    
+    def test_another_product_has__sell_price(self):
+        self.assertEqual(0.75 , self.product1.sell_price)
+
+# product_name
+# product_description
+# stock_on_hand
+# out_of_stock
+# cost_price
+# sell_price
 
 
 
