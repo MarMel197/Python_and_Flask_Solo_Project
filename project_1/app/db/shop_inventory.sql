d9c06d8607b07dbe6e7c3e6096260ea0bcec4000
Mark <markmeldrum70@gmail.com>
2020-11-07 10:45:30 +0000

    database setup

---
+++ project_1/app/db/shop_inventory.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS manufacturer;
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255),
+    product_description TEXT,
+    stock_on_hand INT,
+    out_of_stock BOOLEAN,
+    item_cost FLOAT,
+    item_sell FLOAT,
+    product_id INT REFERENCES products(id)
+);
+
+CREATE TABLE manufacturers (
+    id SERIAL PRIMARY KEY,
+    manufacturer_name VARCHAR(255),
+    product_type VARCHAR(255)
+);
