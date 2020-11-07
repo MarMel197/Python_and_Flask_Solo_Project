1e5d591baba2e9ea0ef873b02f31879d77748da4
Mark <markmeldrum70@gmail.com>
2020-11-07 13:26:33 +0000

    All tests complete and working

--- project_1/tests/manufacturer_test.py
+++ project_1/tests/manufacturer_test.py
@@ -5,13 +5,19 @@ class TestManufacturer(unittest.TestCase):
     
     def setUp(self):
         self.manufacturer = Manufacturer('Heinz', 'Food products')
-        self.manufacturer1 = Manufacturer('Baxters', 'Food products')
+        self.manufacturer1 = Manufacturer('Baxters', 'Canned food products')
     
     def test_manufacturer_has_name(self):
         self.assertEqual('Heinz', self.manufacturer.manufacturer_name)
     
     def test_another_manufacturer_has__name(self):
         self.assertEqual('Baxters', self.manufacturer1.manufacturer_name)
+
+    def test_manufacturer_has_type(self):
+        self.assertEqual('Food products', self.manufacturer.product_type)
+    
+    def test_another_manufacturer_has__type(self):
+        self.assertEqual('Canned food products', self.manufacturer1.product_type)
 
 # manufacturer_name
 # product_type
