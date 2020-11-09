fa6278dffe148e3e938ba95146f93bff5812bcd1
Mark <markmeldrum70@gmail.com>
2020-11-09 11:27:35 +0000

    Product controller - untested

--- project_1/app/controllers/product_controller.py
+++ project_1/app/controllers/product_controller.py
@@ -7,10 +7,12 @@ import app.repositories.manufacturer_repository as manufacturer_repository
 
 products_blueprint = Blueprint('products',__name__)
 
+
 @products_blueprint.route('/products')
 def products():
     products = product_repository.select_all()
     return render_template('products/index.html', all_products = products)
+
 
 @products_blueprint.route('/products', methods=['POST'])
 def create_product():
@@ -25,6 +27,40 @@ def create_product():
 
     product_repository.save(product)
     return redirect('/products')
+
+
+@products_blueprint.route('/products/<id>', methids=['GET'])
+def show_product(id):
+    product = product_repository.select(id)
+    return render_template('products/find_product.html', product = product)
+
+@products_blueprint.route('/product/<id>/edit', methods=['GET'])
+def edit_product(id):
+    product = product_repository.select(id)
+    manufacturer = manufacturer_repository.select_all()
+    return render_template('products/edit_product.html', product = product, all_manufacturers = manufacturers)
+
+
+@products_blueprint.route('/products/<id>', methods=['POST'])
+def  update_product(id):
+    product      = request.form['product']
+    description  = request.form['description']
+    inventory    = request.form['inventory']
+    cost         = request.form['cost']
+    sell         = request.form['sell']
+
+    manufacturer = manufacturer_repository.select(manufacturer_id)
+    product      = Product(product, description, inventory, cost, sell)
+
+    product_repository.update(product)
+    return redirect('/products')
+
+
+@products_blueprint.route('/product/<id>/delete', methods=['POST'])
+def delete_product(id):
+    product_repository.delete(id)
+    return redirect('/products')
+
 
 # product_name
 # product_description
