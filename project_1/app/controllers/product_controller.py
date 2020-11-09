3c52f248a765f73d426da5291ffd4cb6ddc82ead
Mark <markmeldrum70@gmail.com>
2020-11-09 10:49:04 +0000

    Create Product controller

--- project_1/app/controllers/product_controller.py
+++ project_1/app/controllers/product_controller.py
@@ -12,4 +12,24 @@ def products():
     products = product_repository.select_all()
     return render_template('products/index.html', all_products = products)
 
-    
+@products_blueprint.route('/products', methods=['POST'])
+def create_product():
+    product      = request.form['product']
+    description  = request.form['description']
+    inventory    = request.form['inventory']
+    cost         = request.form['cost']
+    sell         = request.form['sell']
+
+    manufacturer = manufacturer_repository.select(manufacturer_id)
+    product      = Product(product, description, inventory, cost, sell)
+
+    product_repository.save(product)
+    return redirect('/products')
+
+# product_name
+# product_description
+# stock_on_hand
+# out_of_stock
+# cost_price
+# sell_price
+
